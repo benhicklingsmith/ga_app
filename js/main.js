fc4aa17073b0e42e7e0a1071a56934a8bff4d13f
William Stenner <wjastenner@gmail.com>
2019-03-07 11:00:21 +0000

    Login Page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$("#cantFindID").click(function() {
+  $(".findIDForm")
+    .addClass("show")
+    .removeClass("hidden");
+});
 
-
+$("#closeIDForm").click(function() {
+  $(".findIDForm")
+    .addClass("hidden")
+    .removeClass("show");
+});
