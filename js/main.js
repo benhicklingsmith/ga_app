6a9daea73d6a5eace39bbd2a1e0e98b0a598fe1e
William Stenner <wjastenner@gmail.com>
2019-03-12 16:24:52 +0000

    JS for website navigation

--- js/main.js
+++ js/main.js
@@ -1,78 +1,82 @@
-$(function(){
-    document.getElementById("cantFindID").addEventListener("click", function() {
-  findID();
+$(function() {
+  document.getElementById("cantFindID").addEventListener("click", function() {
+    switchPages("login", "findID");
+  });
 });
-})
 
-function switchPages(from, to){
-    $("." + from).removeClass("current");
-    $("." + to).addClass("current");
+function setPath(path) {
+  $("#pathBtn").removeAttr("onclick");
+  $("#pathBtn").attr("onclick", "switchPages('options', '" + path + "')");
 }
 
-function findID() {
-  $(".login").removeClass("current");
-  $(".findID").addClass("current");
-  $("#footerBtn1").removeAttr("onclick");
-  $("#footerBtn1").attr("onclick", "login('fromID')");
+function switchPages(from, to) {
+  $("." + from).removeClass("current");
+  $("." + to).addClass("current");
 }
 
-function closeFindID() {
-  $(".findID").removeClass("current");
-  $(".login").addClass("current");
-  $("#footerBtn1").removeAttr("onclick");
-  $("#footerBtn1").attr("onclick", "login('fromLogin')");
-}
+// function findID() {
+//   $(".login").removeClass("current");
+//   $(".findID").addClass("current");
+//   $("#footerBtn1").removeAttr("onclick");
+//   $("#footerBtn1").attr("onclick", "login('fromID')");
+// }
 
-function login(from) {
-  if (from === "fromLogin") {
-    var idExists = true;
-    //   if the database contains the staff id from the login page then set the local/session storage, add current to the app section and change the login button to 'previous'. If it doesn't add a 'can't find id, please try again' underneath the staff id input
-    if (idExists) {
-      $(".login").removeClass("current");
-      $(".app").addClass("current");
-      $("#footerBtn1").empty();
-      $("#footerBtn1").append("Previous");
-      $("#footerBtn1").removeAttr("onclick");
-      $("#footerBtn1").attr("onclick", "previous('loginPage')");
-    }
-  } else if (from === "fromID") {
-    $(".findID").removeClass("current");
-    $(".app").addClass("current");
-    $("#footerBtn1").empty();
-    $("#footerBtn1").append("Previous");
-    $("#footerBtn1").removeAttr("onclick");
-    $("#footerBtn1").attr("onclick", "previous('loginPage')");
-  }
-}
+// function closeFindID() {
+//   $(".findID").removeClass("current");
+//   $(".login").addClass("current");
+//   $("#footerBtn1").removeAttr("onclick");
+//   $("#footerBtn1").attr("onclick", "login('fromLogin')");
+// }
 
-function previous(to) {
-  if (to === "loginPage") {
-    $(".app").removeClass("current");
-    $(".login").addClass("current");
-    $("#footerBtn1").removeAttr("onclick");
-    $("#footerBtn1").attr("onclick", "login('fromLogin')");
-    $("#footerBtn1").empty();
-    $("#footerBtn1").append("Login");
-  }
-}
+// function login(from) {
+//   if (from === "fromLogin") {
+//     var idExists = true;
+//   if the database contains the staff id from the login page then set the local/session storage, add current to the app section and change the login button to 'previous'. If it doesn't add a 'can't find id, please try again' underneath the staff id input
+//     if (idExists) {
+//       $(".login").removeClass("current");
+//       $(".app").addClass("current");
+//       $("#footerBtn1").empty();
+//       $("#footerBtn1").append("Previous");
+//       $("#footerBtn1").removeAttr("onclick");
+//       $("#footerBtn1").attr("onclick", "previous('loginPage')");
+//     }
+//   } else if (from === "fromID") {
+//     $(".findID").removeClass("current");
+//     $(".app").addClass("current");
+//     $("#footerBtn1").empty();
+//     $("#footerBtn1").append("Previous");
+//     $("#footerBtn1").removeAttr("onclick");
+//     $("#footerBtn1").attr("onclick", "previous('loginPage')");
+//   }
+// }
+
+// function previous(to) {
+//   if (to === "loginPage") {
+//     $(".app").removeClass("current");
+//     $(".login").addClass("current");
+//     $("#footerBtn1").removeAttr("onclick");
+//     $("#footerBtn1").attr("onclick", "login('fromLogin')");
+//     $("#footerBtn1").empty();
+//     $("#footerBtn1").append("Login");
+//   }
+// }
 
 function test(input) {
-    // convert the parameters to a JSON data string
-    
-    var json = JSON.stringify(input);
-    
-    $.ajax({
-        url: "http://localhost:8081/get_staff",
-        type: "POST",
-        data: json,
-        success: function (rt) {
-            var json = JSON.parse(rt);
-            console.log(json);
-        },
-        error: function () {
-            console.log("error");
-            //alert("error");
-        }
-    });
+  // convert the parameters to a JSON data string
+
+  var json = JSON.stringify(input);
+
+  $.ajax({
+    url: "http://localhost:8081/get_staff",
+    type: "POST",
+    data: json,
+    success: function(rt) {
+      var json = JSON.parse(rt);
+      console.log(json);
+    },
+    error: function() {
+      console.log("error");
+      //alert("error");
+    }
+  });
 }
-;
