9df1a2bdfddc066020188b48e6a431c91aba9d01
Ben Smith <benhicklingsmith@gmail.com>
2019-03-12 10:17:57 +0000

    added new code from will

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-$("#cantFindID").click(function () {
-    $(".findIDForm")
-            .addClass("show")
-            .removeClass("hidden");
-});
-
-$("#closeIDForm").click(function () {
-    $(".findIDForm")
-            .addClass("hidden")
-            .removeClass("show");
-});
-
 function test(input) {
     // convert the parameters to a JSON data string
     
@@ -30,3 +18,54 @@ function test(input) {
     });
 }
 ;
+
+document.getElementById("cantFindID").addEventListener("click", function() {
+  findID();
+});
+
+function findID() {
+  $(".login").removeClass("current");
+  $(".findID").addClass("current");
+  $("#footerBtn1").removeAttr("onclick");
+  $("#footerBtn1").attr("onclick", "login('fromID')");
+}
+
+function closeFindID() {
+  $(".findID").removeClass("current");
+  $(".login").addClass("current");
+  $("#footerBtn1").removeAttr("onclick");
+  $("#footerBtn1").attr("onclick", "login('fromLogin')");
+}
+
+function login(from) {
+  if (from === "fromLogin") {
+    var idExists = true;
+    //   if the database contains the staff id from the login page then set the local/session storage, add current to the app section and change the login button to 'previous'. If it doesn't add a 'can't find id, please try again' underneath the staff id input
+    if (idExists) {
+      $(".login").removeClass("current");
+      $(".app").addClass("current");
+      $("#footerBtn1").empty();
+      $("#footerBtn1").append("Previous");
+      $("#footerBtn1").removeAttr("onclick");
+      $("#footerBtn1").attr("onclick", "previous('loginPage')");
+    }
+  } else if (from === "fromID") {
+    $(".findID").removeClass("current");
+    $(".app").addClass("current");
+    $("#footerBtn1").empty();
+    $("#footerBtn1").append("Previous");
+    $("#footerBtn1").removeAttr("onclick");
+    $("#footerBtn1").attr("onclick", "previous('loginPage')");
+  }
+}
+
+function previous(to) {
+  if (to === "loginPage") {
+    $(".app").removeClass("current");
+    $(".login").addClass("current");
+    $("#footerBtn1").removeAttr("onclick");
+    $("#footerBtn1").attr("onclick", "login('fromLogin')");
+    $("#footerBtn1").empty();
+    $("#footerBtn1").append("Login");
+  }
+}
