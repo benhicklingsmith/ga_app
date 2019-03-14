4584c0dcbc3762e020ed8e06683a0dc9f3a52448
William Stenner <wjastenner@gmail.com>
2019-03-14 23:31:10 +0000

    setFaultType, checkInput functions

--- js/main.js
+++ js/main.js
@@ -2,15 +2,18 @@ $(function () {
     document.getElementById("cantFindID").addEventListener("click", function () {
         switchPages("login", "findID");
     });
+    $('#otherCategory').on("change keyup paste", function () {
+        setFaultType('other');
+    });
 });
 
 
 function setPath(path) {
     $("#pathBtn").removeAttr("onclick");
     $("#pathBtn").attr(
-            "onclick",
-            "switchPages('options', '" + path + "'); clearPath()"
-            );
+        "onclick",
+        "switchPages('options', '" + path + "'); clearPath()"
+    );
 }
 
 function clearPath() {
@@ -57,20 +60,18 @@ function clearIssues() {
 }
 
 // Run this function on success of AJAX and pass it the outcome:
-// - first check whether the carriage number exists
-// - if it does it sets the local storage, removes any previous issue messages
-// - and sets the location input of step 4
+// - if the carriage number exists set the local storage, remove any previous issue messages, set the fault options available to the user in step 2 and set the location input of step 4
 // - if the carriage number doesn't exist it displays 'can't find carriage number' message
 function checkCar(carExists) {
     // temp carExists variable
     var carExists = true;
     if (carExists) {
         var carDetails = new Object();
-        carDetails.seats = 56;
-        carDetails.wifi = true;
-        carDetails.sockets = true;
-        carDetails.displayPanel = true;
+        carDetails.seats = 0;
         carDetails.toilet = true;
+        carDetails.displayPanel = true;
+        carDetails.socket = true;
+        carDetails.wifi = false;
         localStorage.setItem("carDetails", JSON.stringify(carDetails));
         $(".issue").removeClass("show");
         switchPages("rf-1", "rf-2");
@@ -81,6 +82,28 @@ function checkCar(carExists) {
             $("#region").addClass("show");
             $("#seats").removeClass("show");
         }
+        $(".faultOption").removeClass("hide");
+        for (var key in carDetails) {
+            if (typeof carDetails[key] === "boolean") {
+                if (!carDetails[key]) {
+                    $("#" + key).addClass('hide');
+                }
+            }
+        }
+
+        var hiddenOptions = $('.faultOption.hide').length;
+        var totalOptions = $('.faultOptions').children().length;
+        var options = totalOptions - hiddenOptions;
+        var optionParity = options % 2;
+
+        if (options > 6) {
+            $('.faultOption').css("height", "50px");
+        }
+
+        if (optionParity === 1) {
+            $('#other').css("grid-column", "span 2");
+        }
+
     } else {
         $(".issue").addClass("show");
     }
@@ -118,10 +141,10 @@ function typeNum(num) {
     //get the current number shown on the screen
     var seatNoStr = $("#seatNo").text();
     var currentSeatStr = seatNoStr.split(": ").pop();
-    
+
     //append new digit
     var newSeatStr = currentSeatStr + num;
-    
+
     //parse to int to allow for comparison with max seat number
     var newSeatInt = parseInt(newSeatStr);
 
@@ -142,7 +165,34 @@ function typeNum(num) {
     }
 }
 
+// Called when the user selects any fault option button or when any change is made to the description panel. Removes show class from all fault options and adds a show class to the selected button (used to change the css). If the selected button is not other, or it is other and the description is not empty, then set the switchPage function for the next button. If other is selected and input is empty reset the onlclick function of the rf-2Next to checkInput
+
 function setFaultType(type) {
     $(".faultOption").removeClass("show");
     $("#" + type).addClass('show');
+    var otherInput = $('#otherCategory').val();
+
+    // set the fault locating method
+    // set next button on click switchPages function
+    if (type !== "other" || (type === 'other' && otherInput !== '')) {
+        $("#rf-2Next").attr(
+            "onclick",
+            "checkInput(); switchPages('rf-2', 'rf-3')"
+        );
+    } else if (type === 'other') {
+        $('#rf-2Next').removeAttr('onlick');
+        $("#rf-2Next").attr(
+            "onclick",
+            "checkInput()"
+        );
+    }
+}
+
+// Called when the user selects 'Next'. Checks to ensure that an option has been selected in step 2 and if 'other' is selected it checks to make sure a description is provided
+function checkInput() {
+    if ($('#other').hasClass('show') && $('#otherCategory').val() === '') {
+        alert('Please enter a description');
+    } else if ($('.faultOption.show').length === 0) {
+        alert('Please select an option');
+    }
 }
